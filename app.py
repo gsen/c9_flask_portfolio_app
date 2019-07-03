54a9f8c50ebd1e8123f5ac0d338721f6136d6e3f
gsen <gaurav.s3n@gmail.com>
2019-07-03 22:35:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + "likes to code in python"
+	new_name = name + " likes to code in python"
 	return render_template('index.html', name=new_name)
 
 
